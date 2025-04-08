98719b0bb6249229a1b0801b1ba17fd45bd772e2
Austin Philip <34815153+TinCanAustin@users.noreply.github.com>
2025-04-08 14:40:25 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-export default defineConfig({
-  base: "/group10-finalproject/", 
+export default defineConfig({ 
   plugins: [react()],
   resolve: {
     alias: {
